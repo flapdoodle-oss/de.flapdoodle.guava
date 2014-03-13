4263eaf5a74824fdead01822bcc2e3989b2d7094
Michael Mosmann <michael@mosmann.de>
2014-03-13 13:46:03 +0100

    imports

--- src/main/java/de/flapdoodle/guava/MapTransformations.java
+++ src/main/java/de/flapdoodle/guava/MapTransformations.java
@@ -1,28 +1,23 @@
 /**
- * Copyright (C) 2013
- *   Michael Mosmann <michael@mosmann.de>
+ * Copyright (C) 2013 Michael Mosmann <michael@mosmann.de>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package de.flapdoodle.guava;
 
-import java.util.Map;
-
 import com.google.common.base.Function;
-import com.google.common.base.Preconditions;
-
 import de.flapdoodle.guava.functions.NoTransformation;
-import java.util.Set;
+import java.util.Map;
 
 public abstract class MapTransformations {
 
@@ -31,7 +26,7 @@ public abstract class MapTransformations {
 	}
 
 	public static <S, D, V, M> Map<D, M> transform(Map<S, V> map, final Function<? super S, D> keytransformation,
-			final Foldleft<? super V, M> valuetransformation) {
+					final Foldleft<? super V, M> valuetransformation) {
 		return Transformations.map(map.entrySet(), new Function<Map.Entry<S, V>, D>() {
 
 			@Override
@@ -53,14 +48,14 @@ public abstract class MapTransformations {
 	}
 
 	public static <S, D, V, M> Map<D, M> transform(Map<S, V> map, final Function<? super S, D> keytransformation,
-			final Function<? super V, M> valuetransformation) {
+					final Function<? super V, M> valuetransformation) {
 		return transform(map, keytransformation, new Folds.ValueFromLeftIllegalFold<V, M>(valuetransformation));
 	}
 
 	public static <S, D, V> Map<D, V> transformKeys(Map<S, V> map, final Function<? super S, D> keytransformation) {
 		return transform(map, keytransformation, new NoTransformation<V>());
 	}
-	
+
 	public static <K, V> Map<V, K> swap(Map<K, V> map) {
 		return Transformations.map(map.entrySet(), new Function<Map.Entry<K, V>, V>() {
 
@@ -68,14 +63,14 @@ public abstract class MapTransformations {
 			public V apply(Map.Entry<K, V> input) {
 				return input.getValue();
 			}
-			
-		},new Function<Map.Entry<K, V>, K>() {
+
+		}, new Function<Map.Entry<K, V>, K>() {
 
 			@Override
 			public K apply(Map.Entry<K, V> input) {
 				return input.getKey();
 			}
-			
+
 		});
 	}
 }
